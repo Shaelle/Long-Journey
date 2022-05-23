5482f1ce36f3737b61568aa95ee0814bb55abd95
Olga Panisova <shadenoff@gmail.com>
2022-05-23 16:45:42 +0300

    Added water tiles. Food can now be replenishable.

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -2,21 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Food : MonoBehaviour
 {
     [SerializeField] [Range(1,100)] float amount = 25;
 
+    [SerializeField] [Range(0, 50)] float minTimer = 5;
+    [SerializeField] [Range(0, 50)] float maxTimer = 30;
+
+    [SerializeField] bool singleTimeUse = false;
+
+    float timer = 0;
+
+    new SpriteRenderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        PlayerConditions player = collision.GetComponent<PlayerConditions>();
-
-        if (player != null)
+        if (renderer.enabled)
         {
-            player.Eat(amount);
-            gameObject.SetActive(false);
+            PlayerConditions player = collision.GetComponent<PlayerConditions>();
+
+            if (player != null)
+            {
+                player.Eat(amount);
+
+                if (singleTimeUse) gameObject.SetActive(false);
+                else StartCoroutine(Timer());              
+            }
         }
+    }
+
+
+    IEnumerator Timer()
+    {
+        renderer.enabled = false;
+        yield return new WaitForSeconds(Random.Range(minTimer, maxTimer));
+        renderer.enabled = true;
     }
 
 }
